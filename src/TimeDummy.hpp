cb783f3ea8475f4a451ae484b10902cd3d8e9e92
dominikpalatynski <palatynski.dominik@gmail.com>
2023-08-23 22:48:17 +0200

    homework2

--- src/TimeDummy.hpp
+++ src/TimeDummy.hpp
@@ -1,24 +1,30 @@
 #pragma once
 #include <chrono>
 #include <thread>
+#include <string>
+#include <tuple>
+#include <vector>
 
+#include <memory>
 
-class TimeDelay {
-    public:
-        virtual ~TimeDelay() = default;
-        virtual void sleepFor(std::chrono::milliseconds duration) = 0;
+class TimeDelay
+{
+public:
+    virtual ~TimeDelay() = default;
+    virtual void sleepFor(std::chrono::milliseconds duration) = 0;
 };
 
-class RealTimeDelay : public TimeDelay {
-    public:
-       
-        virtual void sleepFor(std::chrono::milliseconds duration) override{
-           std::this_thread::sleep_for(duration);
-        };
+class RealTimeDelay : public TimeDelay
+{
+public:
+    virtual void sleepFor(std::chrono::milliseconds duration) override
+    {
+        std::this_thread::sleep_for(duration);
+    };
 };
 
-class SimulatedTimeDelay : public TimeDelay {
-    public:
-       
-        virtual void sleepFor(std::chrono::milliseconds duration) override;
+class SimulatedTimeDelay : public TimeDelay
+{
+public:
+    virtual void sleepFor(std::chrono::milliseconds duration) override{};
 };
--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -15,9 +15,8 @@ struct PizzeriaTest : public ::testing::Test
 {
 public:
     std::unique_ptr<SimulatedTimeDelay> timeDelay = std::make_unique<SimulatedTimeDelay>();
-    Pizzeria pizzeria = Pizzeria("dummyName",std::move(timeDelay)); 
+    Pizzeria pizzeria = Pizzeria("dummyName", std::move(timeDelay));
 };
-
 
 TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55)
 {
@@ -54,17 +53,32 @@ TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55)
 // }
 
 TEST_F(PizzeriaTest, calculatePriceForPizzaMock)
-{   
+{
     // Given
     auto mock = std::make_unique<PizzaMock>();
     Pizzas pizzas = {mock.get()}; // Get the raw pointer from unique_ptr
     EXPECT_CALL(*mock, getPrice())
         .WillOnce(testing::Return(40)); // Set the expected return value
-    
+
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
     auto price = pizzeria.calculatePrice(orderId);
-   
+
     // Then
     ASSERT_EQ(40, price);
+}
+
+TEST_F(PizzeriaTest, Testing2Pizzas)
+{
+    auto mock1 = std::make_unique<PizzaMock>();
+    auto mock2 = std::make_unique<PizzaMock>();
+    auto stub1 = std::make_unique<PizzaStub>("stub");
+
+    EXPECT_CALL(*mock1, getPrice()).Times(2);
+    Pizzas pizzas = {mock1.get(), mock2.get(), stub1.get()};
+
+    auto orderId = pizzeria.makeOrder(pizzas);
+    auto price = pizzeria.calculatePrice(orderId);
+
+    ASSERT_EQ(10, price);
 }
